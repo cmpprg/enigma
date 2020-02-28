7d8f0c3c11897fc575f626dd449b0d870d465f1d
Ryan Camp <cmpprg@icloud.com>
2020-02-27 19:56:26 -0700

    Make decision to create encryption class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,8 @@
+require_relative "encryption"
 class Enigma
-  
+
+  def encrypt(message, key, date)
+    encrypt = Encryption.new(message, key, date)
+    encrypt.encrypted_hash
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -23,11 +23,15 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_decrypt_a_message
+    skip
+    enigma = Enigma.new
+
       expected = {
                 decryption: "hello world",
                 key: "02715",
                 date: "040895"
                 }
+
     assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
   end
 end
