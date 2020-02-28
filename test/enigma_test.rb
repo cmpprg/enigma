92839991fb4891d866f28cda053c2c2b94ec2fe6
Ryan Camp <cmpprg@icloud.com>
2020-02-27 17:54:38 -0700

    Set up encryption and decryption tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,4 +9,25 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_can_encrypt_a_message
+    enigma = Enigma.new
+
+    expected = {
+              encryption: "keder ohulw",
+              key: "02715",
+              date: "040895"
+              }
+
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_a_message
+      expected = {
+                decryption: "hello world",
+                key: "02715",
+                date: "040895"
+                }
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
