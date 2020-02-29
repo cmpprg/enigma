4f06e613d7f8f7073edf3bfa96f154db4de26629
Ryan Camp <cmpprg@icloud.com>
2020-02-29 08:23:05 -0700

    Create #process_key for key processor

--- lib/key_processor.rb
+++ lib/key_processor.rb
@@ -1,7 +1,19 @@
 class KeyProcessor
 
-  attr_reader :input_key
+  attr_reader :input_key, :key_categories
   def initialize(input_key)
     @input_key = input_key
+    @key_categories = "abcd"
+  end
+
+  def process_key
+    key_array = @input_key.split("")
+    counter = 0
+    key_shift = {}
+    key_array.each_cons(2) do |pair|
+      key_shift[key_categories[counter].to_sym] = pair.join.to_i
+      counter += 1
+    end
+    key_shift
   end
 end
--- test/key_processor_test.rb
+++ test/key_processor_test.rb
@@ -1,3 +1,4 @@
+require "simplecov"
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/key_processor"
@@ -13,5 +14,15 @@ class KeyProcessorTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal "02715", @key_processor.input_key
+    assert_equal "abcd", @key_processor.key_categories
+  end
+
+  def test_it_can_process_key
+    expected = {a: 2,
+                b: 27,
+                c: 71,
+                d: 15}
+
+    assert_equal expected, @key_processor.process_key
   end
 end
