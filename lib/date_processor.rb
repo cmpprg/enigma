42082beeead7023e10493eae0ab73a725503428c
Ryan Camp <cmpprg@icloud.com>
2020-02-29 09:27:55 -0700

    Create attr_reader/variables for date processor

--- lib/date_processor.rb
+++ lib/date_processor.rb
@@ -1,6 +1,11 @@
 class DateProcessor
 
+  attr_reader :input_date, :offset_shift_keys,
+              :counter, :offset_shift_values
   def initialize(input_date)
-    
+    @input_date = input_date
+    @offset_shift_keys = "abcd"
+    @counter = 0
+    @offset_shift_values = {}
   end
 end
