15269e59c1d887a37dee9ade059293a088d4fa06
Ryan Camp <cmpprg@icloud.com>
2020-02-29 09:15:06 -0700

    Create existence test for date processor

---
+++ test/date_processor_test.rb
@@ -0,0 +1,15 @@
+require "./test/test_helper"
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/date_processor"
+
+class DateProcessorTest < Minitest::Test
+
+  def setup
+    @date_processor = DateProcessor.new("040895")
+  end
+
+  def test_it_exists
+    assert_instance_of DateProcessor, date_processor
+  end
+end
