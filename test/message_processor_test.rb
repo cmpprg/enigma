19a45080916aaeedd686941bfeab5a4d9674e4cb
Ryan Camp <cmpprg@icloud.com>
2020-02-29 12:53:00 -0700

    Make test to check attributes for message processing

--- test/message_processor_test.rb
+++ test/message_processor_test.rb
@@ -12,4 +12,21 @@ class MessageProcessorTest < Minitest::Test
   def test_it_exists
     assert_instance_of MessageProcessor, @message_processor
   end
+
+  def test_it_has_attributes
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+                  "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                  "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, @message_processor.alphabet
+
+    expected2 = {a: 2, b: 27, c: 71, d: 15}
+
+    assert_equal expected2, @message_processor.key_hash_values
+
+    expected3 = {a:1, b:0, c:2, d:5}
+
+    assert_equal expected3, @message_processor.offset_hash_values
+    assert_equal "hello world", @message_processor.message
+  end
 end
