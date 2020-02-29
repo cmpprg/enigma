2f2a7183446722713122d0ae960fcbd5a21461a3
Ryan Camp <cmpprg@icloud.com>
2020-02-29 14:27:40 -0700

    Create #total_shift_values helper for message processor

--- test/message_processor_test.rb
+++ test/message_processor_test.rb
@@ -23,21 +23,28 @@ class MessageProcessorTest < Minitest::Test
 
     expected2 = {a: 2, b: 27, c: 71, d: 15}
 
-    assert_equal expected2, @message_processor.key_shift
+    assert_equal expected2, @message_processor.key_shift_values
 
     expected3 = {a:1, b:0, c:2, d:5}
 
-    assert_equal expected3, @message_processor.offset_shift
+    assert_equal expected3, @message_processor.offset_shift_values
     assert_equal "hello world", @message_processor.message
     assert_equal false, @message_processor.is_decryption
     assert_equal "", @message_processor.output_message
   end
 
+  def test_it_can_find_total_shift_values
+    expected = {:a=>3, :b=>27, :c=>73, :d=>20}
+
+    assert_equal expected, @message_processor.total_shift_values
+  end
+
   def test_it_can_encrypt_a_message
+
     @message_processor.process_message
     @message_processor2.process_message
 
     assert_equal "keder ohulw", @message_processor.output_message
-    assert_equal "keder1,sprrdx!@%^&*", @message_processor2.output_message
+    assert_equal "keder1,tzojeg!@%^&*", @message_processor2.output_message
   end
 end
