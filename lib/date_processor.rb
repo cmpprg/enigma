dd1ba04d52c304e911dbe4976f44c8f483705242
Ryan Camp <cmpprg@icloud.com>
2020-02-29 09:50:38 -0700

    Create #process_date for date processor

--- lib/date_processor.rb
+++ lib/date_processor.rb
@@ -8,4 +8,17 @@ class DateProcessor
     @counter = 0
     @offset_shift_values = {}
   end
+
+  def process_date
+    square_date = (input_date.to_i ** 2).to_s
+
+    last_four_nums = square_date[-4, 4]
+
+    split_four_nums = last_four_nums.split("")
+
+    x = split_four_nums.each do |num|
+      @offset_shift_values[@offset_shift_keys[@counter].to_sym] = num.to_i
+      @counter += 1
+    end
+  end
 end
