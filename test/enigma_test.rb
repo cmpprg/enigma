a252e8e433646eefff63c237b94296f064e48701
Ryan Camp <cmpprg@icloud.com>
2020-02-29 18:22:52 -0700

    Make test to check it can encrypt a message

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,15 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_can_encrypt_a_message
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+                }
+    encrypt = @enigma.encrypt("hello world", "02715", "040895")
+
+    assert_equal expected, encrypt
+  end
 end
