033d24814b82e71e63b48eff1ffc5aa619d4f71c
Ryan Camp <cmpprg@icloud.com>
2020-02-29 19:09:15 -0700

    Create #output_hash helper for enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,10 @@
 require "./lib/message_processor"
 class Enigma
 
+  def output_hash(processed_message, key, date, action = :encryption)
+    Hash[action, processed_message, :key, key, :date, date]
+  end
+
   def encrypt(input_message, key, date)
     encrypted_message = MessageProcessor.new(input_message, key, date).encrypt
 
@@ -9,7 +13,6 @@ class Enigma
       key: key,
       date: date
     }
-
   end
 
   def decrypt(ciphertext, key, date)
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,6 +13,23 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_build_output_hash
+    expected = {
+                encryption: "whatever",
+                key: "123456",
+                date: "76543"
+                }
+
+    expected2 = {
+                decryption: "whatever",
+                key: "123456",
+                date: "76543"
+                }
+
+    assert_equal expected, @enigma.output_hash("whatever", "123456", "76543")
+    assert_equal expected2, @enigma.output_hash("whatever", "123456", "76543", :decryption)
+  end
+
   def test_it_can_encrypt_a_message
     expected = {
                 encryption: "keder ohulw",
@@ -27,7 +44,9 @@ class EnigmaTest < Minitest::Test
                 }
     encrypt = @enigma.encrypt("hello world", "02715", "040895")
     encrypt2 = @enigma.encrypt("hello1, world!@%^&*", "02715", "040895")
+
     assert_equal expected, encrypt
+    assert_equal expected2, encrypt2
   end
 
   def test_it_can_decrypt_a_message
