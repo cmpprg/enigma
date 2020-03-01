06d05fd0fbcebaf1eb8181ea254ff7f821ed6e00
Ryan Camp <cmpprg@icloud.com>
2020-02-29 18:09:25 -0700

    Make existence test for enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,1 +1,15 @@
+require "./test/test_helper"
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/enigma"
 
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, enigma
+  end
+end
