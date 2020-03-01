be85480c328b976731bedca6ab25f967804bc131
Ryan Camp <cmpprg@icloud.com>
2020-02-29 19:11:05 -0700

    Refactor #encrypt and #decrypt to use #output_hash helper

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,21 +7,11 @@ class Enigma
 
   def encrypt(input_message, key, date)
     encrypted_message = MessageProcessor.new(input_message, key, date).encrypt
-
-    output_hash = {
-      encryption: encrypted_message,
-      key: key,
-      date: date
-    }
+    output_hash(encrypted_message, key, date)
   end
 
   def decrypt(ciphertext, key, date)
     decrypted_message = MessageProcessor.new(ciphertext, key, date).decrypt
-
-    output_hash = {
-      decryption: decrypted_message,
-      key: key,
-      date: date
-    }
+    output_hash(decrypted_message, key, date, :decryption)
   end
 end
