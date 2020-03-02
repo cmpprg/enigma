2c6974c7107340691efcd2c415b6930e553c253e
Ryan Camp <cmpprg@icloud.com>
2020-03-02 13:12:01 -0700

    Refactor runner files to handle invalid input errors

--- decrypt.rb
+++ decrypt.rb
@@ -1,5 +1,17 @@
 require "./lib/enigma"
 
+def error_message
+  puts  """
+        Your encrypt source file is invalid, you should have a txt file with three lines.
+
+        The Message
+        The Key;              '12345', must be 5 digits
+        The Date (optional);  'ddmmyy' must be 6 digits
+
+        Please try again with a valid source txt file.
+        """
+end
+
 enigma = Enigma.new
 from = File.open(ARGV[0], "r")
 to = File.open(ARGV[1], "w")
@@ -7,23 +19,20 @@ to = File.open(ARGV[1], "w")
 info = from.readlines.map(&:chomp)
 from.close
 
-unless info.nil? || info.length > 3
-  results = enigma.decrypt(info[0], info[1], info[2]) if info.length == 3
-  results = enigma.decrypt(info[0], info[1]) if info.length == 2
+unless info == [""] || info.length > 3
+  results = enigma.encrypt(info[0], info[1], info[2]) if info.length == 3
+  results = enigma.encrypt(info[0], info[1]) if info.length == 2
 
-  results.each do |key, info|
-    to.write("#{info}\n")
+  if results == "Invalid Input"
+    error_message
+  else
+    results.each do |key, info|
+      to.write("#{info}\n")
+    end
+    to.close
+    puts "Created '#{ARGV[1]}' with the key #{results[:key]} and date #{results[:date]}"
+
   end
-  to.close
-
-  puts "Created '#{ARGV[1]}' with the key #{results[:key]} and date #{results[:date]}"
-
 else
-  puts "Your encrypt source file is invalid, you should have a txt file with three lines.
-
-        The Message
-        The Key (optional)
-        The Date (optional)
-
-        Please try again with a proper source txt file."
+  error_message
 end
--- encrypt.rb
+++ encrypt.rb
@@ -1,31 +1,37 @@
 require "./lib/enigma"
 
+def error_message
+  puts "Your encrypt source file is invalid, you should have a txt file with three lines.
+
+        The Message
+        The Key (optional); '12345', must be 5 digits
+        The Date (optional); 'ddmmyy' must be 6 digits
+
+        Please try again with a proper source txt file."
+end
+
 enigma = Enigma.new
 from = File.open(ARGV[0], "r")
 to = File.open(ARGV[1], "w")
 
 info = from.readlines.map(&:chomp)
 from.close
-p info
 
 unless info == [""] || info.length > 3
   results = enigma.encrypt(info[0], info[1], info[2]) if info.length == 3
   results = enigma.encrypt(info[0], info[1]) if info.length == 2
   results = enigma.encrypt(info[0]) if info.length == 1
 
-  results.each do |key, info|
-    to.write("#{info}\n")
+  if results == "Invalid Input"
+    error_message
+  else
+    results.each do |key, info|
+      to.write("#{info}\n")
+    end
+    to.close
+    puts "Created '#{ARGV[1]}' with the key #{results[:key]} and date #{results[:date]}"
+    
   end
-  to.close
-
-  puts "Created '#{ARGV[1]}' with the key #{results[:key]} and date #{results[:date]}"
-
 else
-  puts "Your encrypt source file is invalid, you should have a txt file with three lines.
-
-        The Message
-        The Key (optional)
-        The Date (optional)
-
-        Please try again with a proper source txt file."
+  error_message
 end
