a89ec8b9a4e7cfb1204bcbe0f46d44e71bacf13e
Ryan Camp <cmpprg@icloud.com>
2020-03-02 10:07:33 -0700

    Clean up code a bit, fix random number generator for key value

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,12 +2,28 @@ require "date"
 require "./lib/message_processor"
 class Enigma
 
+  def encrypt(input_message, key = random_five, date = todays_date)
+    return "Invalid Input" if !valid_inputs?(input_message, key, date)
+    encrypted_message = MessageProcessor.new(input_message, key, date).encrypt
+    output_hash(encrypted_message, key, date)
+  end
+
+  def decrypt(ciphertext, key, date = todays_date)
+    return "Invalid Input" if !valid_inputs?(ciphertext, key, date)
+    decrypted_message = MessageProcessor.new(ciphertext, key, date).decrypt
+    output_hash(decrypted_message, key, date, :decryption)
+  end
+
   def output_hash(processed_message, key, date, action = :encryption)
     Hash[action, processed_message, :key, key, :date, date]
   end
 
   def random_number
-    (0..9).to_a.sample(5).shuffle.join
+    [*"0".."9"].shuffle.sample
+  end
+
+  def random_five
+    [*1..5].map{|num| random_number}.join
   end
 
   def todays_date
@@ -22,23 +38,10 @@ class Enigma
 
   def valid_key?(key)
     return false unless key.length == 5
-    return false unless key.split("").all?{ |num| ("0".."9").include?(num)}
-    true
+    key.split("").all?{ |num| ("0".."9").include?(num)}
   end
 
   def valid_inputs?(message, key, date)
     message.class == String && valid_date?(date) && valid_key?(key)
-  end
-
-  def encrypt(input_message, key = random_number, date = todays_date)
-    return "Invalid Input" if !valid_inputs?(input_message, key, date)
-    encrypted_message = MessageProcessor.new(input_message, key, date).encrypt
-    output_hash(encrypted_message, key, date)
-  end
-
-  def decrypt(ciphertext, key, date = todays_date)
-    return "Invalid Input" if !valid_inputs?(ciphertext, key, date)
-    decrypted_message = MessageProcessor.new(ciphertext, key, date).decrypt
-    output_hash(decrypted_message, key, date, :decryption)
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,6 +14,48 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_encrypt_a_message
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+                }
+
+    expected2 = {
+                encryption: "keder1,tzojeg!@%^&*",
+                key: "02715",
+                date: "040895"
+                }
+    encrypt = @enigma.encrypt("hello world", "02715", "040895")
+    encrypt2 = @enigma.encrypt("hello1, world!@%^&*", "02715", "040895")
+    encrypt3 = @enigma.encrypt(false, "02715", "040895")
+
+    assert_equal expected, encrypt
+    assert_equal expected2, encrypt2
+    assert_equal "Invalid Input", encrypt3
+  end
+
+  def test_it_can_decrypt_a_message
+    expected = {
+                decryption: "hello world",
+                key: "02715",
+                date: "040895"
+                }
+
+    expected2 = {
+                decryption: "hello1, world!@%^&*",
+                key: "02715",
+                date: "040895"
+                }
+    decrypt = @enigma.decrypt("keder ohulw", "02715", "040895")
+    decrypt2 = @enigma.decrypt("keder1,tzojeg!@%^&*", "02715", "040895")
+    decrypt3 = @enigma.decrypt("keder1,tzojeg!@%^&*", "0S715", "040895")
+
+    assert_equal expected, decrypt
+    assert_equal expected2, decrypt2
+    assert_equal "Invalid Input", decrypt3
+  end
+
   def test_it_can_build_output_hash
     expected = {
                 encryption: "whatever",
@@ -31,14 +73,20 @@ class EnigmaTest < Minitest::Test
     assert_equal expected2, @enigma.output_hash("whatever", "12345", "76543", :decryption)
   end
 
-  def test_it_can_generate_random_five_digit_number
-    assert_equal 5, @enigma.random_number.length
+  def test_it_can_generate_random_number
+    assert_equal 1, @enigma.random_number.length
     assert_instance_of String, @enigma.random_number
-    assert_equal true, ("0".."9").include?(@enigma.random_number[0])
-    assert_equal true, ("0".."9").include?(@enigma.random_number[1])
-    assert_equal true, ("0".."9").include?(@enigma.random_number[2])
-    assert_equal true, ("0".."9").include?(@enigma.random_number[3])
-    assert_equal true, ("0".."9").include?(@enigma.random_number[4])
+    assert_equal true, ("0".."9").include?(@enigma.random_number)
+  end
+
+  def test_it_can_collect_a_random_key_of_five_numbers
+    assert_equal 5, @enigma.random_five.length
+    assert_instance_of String, @enigma.random_five
+    assert_equal true, ("0".."9").include?(@enigma.random_five[0])
+    assert_equal true, ("0".."9").include?(@enigma.random_five[1])
+    assert_equal true, ("0".."9").include?(@enigma.random_five[2])
+    assert_equal true, ("0".."9").include?(@enigma.random_five[3])
+    assert_equal true, ("0".."9").include?(@enigma.random_five[4])
   end
 
   def test_that_it_can_put_gather_todays_date_in_ddmmyy_format
@@ -86,48 +134,6 @@ class EnigmaTest < Minitest::Test
     assert_equal false, @enigma.valid_inputs?(true, "12345", "121212")
   end
 
-  def test_it_can_encrypt_a_message
-    expected = {
-                encryption: "keder ohulw",
-                key: "02715",
-                date: "040895"
-                }
-
-    expected2 = {
-                encryption: "keder1,tzojeg!@%^&*",
-                key: "02715",
-                date: "040895"
-                }
-    encrypt = @enigma.encrypt("hello world", "02715", "040895")
-    encrypt2 = @enigma.encrypt("hello1, world!@%^&*", "02715", "040895")
-    encrypt3 = @enigma.encrypt(false, "02715", "040895")
-
-    assert_equal expected, encrypt
-    assert_equal expected2, encrypt2
-    assert_equal "Invalid Input", encrypt3
-  end
-
-  def test_it_can_decrypt_a_message
-    expected = {
-                decryption: "hello world",
-                key: "02715",
-                date: "040895"
-                }
-
-    expected2 = {
-                decryption: "hello1, world!@%^&*",
-                key: "02715",
-                date: "040895"
-                }
-    decrypt = @enigma.decrypt("keder ohulw", "02715", "040895")
-    decrypt2 = @enigma.decrypt("keder1,tzojeg!@%^&*", "02715", "040895")
-    decrypt3 = @enigma.decrypt("keder1,tzojeg!@%^&*", "0S715", "040895")
-
-    assert_equal expected, decrypt
-    assert_equal expected2, decrypt2
-    assert_equal "Invalid Input", decrypt3
-  end
-
   def test_it_can_use_default_todays_date_for_encryption
     @enigma.stubs(:todays_date).returns("010320")
 
@@ -144,9 +150,9 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.decrypt("lib sdmcvpu", "02715")
   end
 
-  def test_it_can_use_random_number_and_todays_date_as_defults_for_encrypt
+  def test_it_can_use_random_five_and_todays_date_as_defults_for_encrypt
     @enigma.stubs(:todays_date).returns("010320")
-    @enigma.stubs(:random_number).returns("08351")
+    @enigma.stubs(:random_five).returns("08351")
 
     expected = {encryption: "rktiyfdlarl", key: "08351", date: "010320"}
 
